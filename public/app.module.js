a69cbab023fa72b67b8b7a8eb55cb474e302b9a8
dannohr <danielnohr@gmail.com>
2017-11-10 12:17:21 -0600

    Update

--- public/app.module.js
+++ public/app.module.js
@@ -1,54 +1,52 @@
-
 angular
-    .module('fullstack', ['angularMoment','ui.router', '720kb.datepicker', 'ngFlash', 'angularModalService','chart.js'])
-
-    // Optional configuration
-    .config(['ChartJsProvider', function (ChartJsProvider) {
+  .module("fullstack", [
+    "angularMoment",
+    "ui.router",
+    "720kb.datepicker",
+    "ngFlash",
+    "angularModalService",
+    "chart.js"
+  ])
+  // Optional configuration
+  .config([
+    "ChartJsProvider",
+    function(ChartJsProvider) {
       // Configure all charts
       ChartJsProvider.setOptions({
-        chartColors: ['#FF5252', '#FF8A80'],
-        responsive: true,
-
+        chartColors: ["#FF5252", "#FF8A80"],
+        responsive: true
       });
       // Configure all line charts
-      ChartJsProvider.setOptions('line', {
+      ChartJsProvider.setOptions("line", {
         showLines: true,
         fill: false,
-        tooltipFillColor: '#EEE',
-        tooltipFontColor: '#000',
+        tooltipFillColor: "#EEE",
+        tooltipFontColor: "#000",
         tooltipFontSize: 8,
         tooltipCornerRadius: 8,
         responsive: true
       });
+    }
+  ])
+  .config(function($locationProvider) {
+    $locationProvider.html5Mode(true);
+  });
 
-    //   ChartJsProvider.setOptions({
-    //     tooltipFillColor: '#EEE',
-    //     tooltipFontColor: '#000',
-    //     tooltipFontSize: 8,
-    //     tooltipCornerRadius: 8
-    // });
-    }])
+$(document).ready(function() {
+  //This is used for the nav bar
 
-    
-  $(document).ready(function () {  //This is used for the nav bar
-    
-            // Change Highlight On Nav menu, based on what was clicked
-            $(".nav a").on("click", function(){
-                $(".nav").find(".active").removeClass("active");
-                $(this).parent().addClass("active");
-            });
-    
-    
-            // Collapse mobile menu after selection
-            $(".nav-item-colapse").click(function(event) {
-                $(".navbar-collapse").collapse('hide');
-              });
+  // Change Highlight On Nav menu, based on what was clicked
+  $(".nav a").on("click", function() {
+    $(".nav")
+      .find(".active")
+      .removeClass("active");
+    $(this)
+      .parent()
+      .addClass("active");
+  });
 
-
-              
-         
-           
-          
-    });
-
-   
+  // Collapse mobile menu after selection
+  $(".nav-item-colapse").click(function(event) {
+    $(".navbar-collapse").collapse("hide");
+  });
+});
