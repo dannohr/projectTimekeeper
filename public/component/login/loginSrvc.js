863026d99ede603d200c525b0027d0742296cb41
dannohr <danielnohr@gmail.com>
2017-11-10 11:29:45 -0600

    update

--- public/component/login/loginSrvc.js
+++ public/component/login/loginSrvc.js
@@ -1,5 +1,5 @@
-angular.module('fullstack').service('loginSrvc', function($http, $state) {
-    // you can use this function for every request to get user.
-    // don't write new versions of this in every service, keep it DRY
-    this.getUser = () => $http.get('/auth/me');
+angular.module("fullstack").service("loginSrvc", function($http, $state) {
+  // you can use this function for every request to get user.
+  // don't write new versions of this in every service, keep it DRY
+  this.getUser = () => $http.get("/app1/auth/me");
 });
